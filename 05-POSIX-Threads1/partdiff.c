24a341c52f46d4ac51e9794219866d48f58235f3
Lobsang Gurung <gurung@wr.informatik.uni-hamburg.de>
2025-11-15 18:48:21 +0100

    bitti

--- 05-POSIX-Threads1/partdiff.c
+++ 05-POSIX-Threads1/partdiff.c
@@ -9,7 +9,7 @@
 /** File:      partdiff.c                                                  **/
 /**                                                                        **/
 /** Purpose:   Partial differential equation solver for Gauß-Seidel and    **/
-/**            Jacobi method.                                              **/
+/**            Jacobi method (parallelized with POSIX threads).            **/
 /**                                                                        **/
 /****************************************************************************/
 /****************************************************************************/
@@ -26,6 +26,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/time.h>
+#include <pthread.h>
 
 #include "partdiff.h"
 
@@ -42,6 +43,27 @@ struct calculation_results {
   uint64_t stat_iteration; /* number of current iteration                    */
   double stat_precision;   /* actual precision of all slaves in iteration    */
 };
+
+/* ************************************************************************ */
+/* Thread-specific structures                                               */
+/* ************************************************************************ */
+
+struct thread_data {
+  int thread_id;
+  int num_threads;
+  const struct calculation_arguments *arguments;
+  struct calculation_results *results;
+  const struct options *options;
+  int *m1;
+  int *m2;
+  int *term_iteration;
+  double *maxResiduum;
+  int *continue_iteration;
+};
+
+/* Global mutex and barrier for thread synchronization */
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_barrier_t barrier;
 
 /* ************************************************************************ */
 /* Global variables                                                         */
@@ -88,7 +110,7 @@ static void *allocateMemory(size_t size) {
   void *p;
 
   if ((p = malloc(size)) == NULL) {
-    printf("Speicherprobleme! (%" PRIu64 " Bytes angefordert)\n", size);
+    printf("Speicherprobleme! (%" PRIu64 " Bytes angefordert)\n", (uint64_t)size);
     exit(1);
   }
 
@@ -152,25 +174,126 @@ static void initMatrices(struct calculation_arguments *arguments,
 }
 
 /* ************************************************************************ */
+/* thread_calculate: Thread function for parallel Jacobi calculation        */
+/* ************************************************************************ */
+static void *thread_calculate(void *arg) {
+  struct thread_data *data = (struct thread_data *)arg;
+  
+  int const N = data->arguments->N;
+  double const h = data->arguments->h;
+  
+  double pih = 0.0;
+  double fpisin = 0.0;
+  
+  if (data->options->inf_func == FUNC_FPISIN) {
+    pih = PI * h;
+    fpisin = 0.25 * TWO_PI_SQUARE * h * h;
+  }
+  
+  /* berechnet die Reihen pro Thread */
+  int total_rows = N - 1;  /* äußeren Zeilen nicht zum rechnen */
+  int rows_per_thread = total_rows / data->num_threads;
+  int rest = total_rows % data->num_threads;
+  
+  /* die ersten Threads gekommen den Rest */
+  int start_row;
+  int end_row;
+  if (data->thread_id < rest) {
+    start_row = 1 + data->thread_id * (rows_per_thread + 1);
+    end_row = start_row + (rows_per_thread + 1);
+  } else {
+    start_row = 1 + rest * (rows_per_thread + 1) + 
+                (data->thread_id - rest) * rows_per_thread;
+    end_row = start_row + rows_per_thread;
+  }
+  
+  while (*data->continue_iteration) {
+    double **Matrix_Out = data->arguments->Matrix[*data->m1];
+    double **Matrix_In = data->arguments->Matrix[*data->m2];
+    
+    double local_maxResiduum = 0.0;
+    
+    /* nur über Threads spezifische Reihen */
+    for (int i = start_row; i < end_row; i++) {
+      double fpisin_i = 0.0;
+      
+      if (data->options->inf_func == FUNC_FPISIN) {
+        fpisin_i = fpisin * sin(pih * (double)i);
+      }
+      
+      /* over all columns */
+      for (int j = 1; j < N; j++) {
+        double star = 0.25 * (Matrix_In[i - 1][j] + Matrix_In[i][j - 1] +
+                              Matrix_In[i][j + 1] + Matrix_In[i + 1][j]);
+        
+        if (data->options->inf_func == FUNC_FPISIN) {
+          star += fpisin_i * sin(pih * (double)j);
+        }
+        
+        if (data->options->termination == TERM_PREC || *data->term_iteration == 1) {
+          double residuum = Matrix_In[i][j] - star;
+          residuum = (residuum < 0) ? -residuum : residuum;
+          local_maxResiduum = (residuum < local_maxResiduum) ? local_maxResiduum : residuum;
+        }
+        
+        Matrix_Out[i][j] = star;
+      }
+    }
+    
+    /* Update global maxResiduum */
+    pthread_mutex_lock(&mutex);
+    if (local_maxResiduum > *data->maxResiduum) {
+      *data->maxResiduum = local_maxResiduum;
+    }
+    pthread_mutex_unlock(&mutex);
+    
+    /* Auf Threads warten */
+    pthread_barrier_wait(&barrier);
+    
+    /* Nur ein Thread!  */
+    if (data->thread_id == 0) {
+      data->results->stat_iteration++;
+      data->results->stat_precision = *data->maxResiduum;
+      
+      /* tausche m1 und m2 */
+      int temp = *data->m1;
+      *data->m1 = *data->m2;
+      *data->m2 = temp;
+      
+      /* checked ob nach interation oder Genauigkeit abgebrochen wird */
+      if (data->options->termination == TERM_PREC) {
+        if (*data->maxResiduum < data->options->term_precision) {
+          *data->continue_iteration = 0;
+        }
+      } else if (data->options->termination == TERM_ITER) {
+        (*data->term_iteration)--;
+        if (*data->term_iteration <= 0) {
+          *data->continue_iteration = 0;
+        }
+      }
+      
+      /* Reset maxResiduum für nächste Iteration */
+      *data->maxResiduum = 0.0;
+    }
+    
+    /* alle Thread warten auf Thread 0 */
+    pthread_barrier_wait(&barrier);
+  }
+  
+  return NULL;
+}
+
+/* ************************************************************************ */
 /* calculate: solves the equation                                           */
 /* ************************************************************************ */
 static void calculate(struct calculation_arguments const *arguments,
                       struct calculation_results *results,
                       struct options const *options) {
-  int i, j;           /* local variables for loops */
   int m1, m2;         /* used as indices for old and new matrices */
-  double star;        /* four times center value minus 4 neigh.b values */
-  double residuum;    /* residuum of current iteration */
   double maxResiduum; /* maximum residuum value of a slave in iteration */
-
-  int const N = arguments->N;
-  double const h = arguments->h;
-
-  double pih = 0.0;
-  double fpisin = 0.0;
-
   int term_iteration = options->term_iteration;
-
+  int continue_iteration = 1;
+  
   /* initialize m1 and m2 depending on algorithm */
   if (options->method == METH_JACOBI) {
     m1 = 0;
@@ -179,64 +302,109 @@ static void calculate(struct calculation_arguments const *arguments,
     m1 = 0;
     m2 = 0;
   }
-
-  if (options->inf_func == FUNC_FPISIN) {
-    pih = PI * h;
-    fpisin = 0.25 * TWO_PI_SQUARE * h * h;
-  }
-
-  while (term_iteration > 0) {
-    double **Matrix_Out = arguments->Matrix[m1];
-    double **Matrix_In = arguments->Matrix[m2];
-
-    maxResiduum = 0;
-
-    /* over all rows */
-    for (i = 1; i < N; i++) {
-      double fpisin_i = 0.0;
-
-      if (options->inf_func == FUNC_FPISIN) {
-        fpisin_i = fpisin * sin(pih * (double)i);
-      }
-
-      /* over all columns */
-      for (j = 1; j < N; j++) {
-        star = 0.25 * (Matrix_In[i - 1][j] + Matrix_In[i][j - 1] +
-                       Matrix_In[i][j + 1] + Matrix_In[i + 1][j]);
-
+  
+  /* für Gauß-Seidel */
+  if (options->method == METH_GAUSS_SEIDEL) {
+    int i, j;
+    int const N = arguments->N;
+    double const h = arguments->h;
+    double pih = 0.0;
+    double fpisin = 0.0;
+    
+    if (options->inf_func == FUNC_FPISIN) {
+      pih = PI * h;
+      fpisin = 0.25 * TWO_PI_SQUARE * h * h;
+    }
+    
+    while (term_iteration > 0) {
+      double **Matrix_Out = arguments->Matrix[m1];
+      double **Matrix_In = arguments->Matrix[m2];
+      
+      maxResiduum = 0;
+      
+      for (i = 1; i < N; i++) {
+        double fpisin_i = 0.0;
+        
         if (options->inf_func == FUNC_FPISIN) {
-          star += fpisin_i * sin(pih * (double)j);
+          fpisin_i = fpisin * sin(pih * (double)i);
         }
-
-        if (options->termination == TERM_PREC || term_iteration == 1) {
-          residuum = Matrix_In[i][j] - star;
-          residuum = (residuum < 0) ? -residuum : residuum;
-          maxResiduum = (residuum < maxResiduum) ? maxResiduum : residuum;
+        
+        for (j = 1; j < N; j++) {
+          double star = 0.25 * (Matrix_In[i - 1][j] + Matrix_In[i][j - 1] +
+                                Matrix_In[i][j + 1] + Matrix_In[i + 1][j]);
+          
+          if (options->inf_func == FUNC_FPISIN) {
+            star += fpisin_i * sin(pih * (double)j);
+          }
+          
+          if (options->termination == TERM_PREC || term_iteration == 1) {
+            double residuum = Matrix_In[i][j] - star;
+            residuum = (residuum < 0) ? -residuum : residuum;
+            maxResiduum = (residuum < maxResiduum) ? maxResiduum : residuum;
+          }
+          
+          Matrix_Out[i][j] = star;
         }
-
-        Matrix_Out[i][j] = star;
+      }
+      
+      results->stat_iteration++;
+      results->stat_precision = maxResiduum;
+      
+      i = m1;
+      m1 = m2;
+      m2 = i;
+      
+      if (options->termination == TERM_PREC) {
+        if (maxResiduum < options->term_precision) {
+          term_iteration = 0;
+        }
+      } else if (options->termination == TERM_ITER) {
+        term_iteration--;
       }
     }
-
-    results->stat_iteration++;
-    results->stat_precision = maxResiduum;
-
-    /* exchange m1 and m2 */
-    i = m1;
-    m1 = m2;
-    m2 = i;
-
-    /* check for stopping calculation depending on termination method */
-    if (options->termination == TERM_PREC) {
-      if (maxResiduum < options->term_precision) {
-        term_iteration = 0;
-      }
-    } else if (options->termination == TERM_ITER) {
-      term_iteration--;
-    }
+    
+    results->m = m2;
   }
-
-  results->m = m2;
+  /* für Jacobi */
+  else {
+    int num_threads = options->number;
+    pthread_t *threads = allocateMemory(num_threads * sizeof(pthread_t));
+    struct thread_data *thread_args = allocateMemory(num_threads * sizeof(struct thread_data));
+    
+    maxResiduum = 0.0;
+    
+    /* Barriere Initianisierung */
+    pthread_barrier_init(&barrier, NULL, num_threads);
+    
+    /* Thread-Erstellung */
+    for (int i = 0; i < num_threads; i++) {
+      thread_args[i].thread_id = i;
+      thread_args[i].num_threads = num_threads;
+      thread_args[i].arguments = arguments;
+      thread_args[i].results = results;
+      thread_args[i].options = options;
+      thread_args[i].m1 = &m1;
+      thread_args[i].m2 = &m2;
+      thread_args[i].term_iteration = &term_iteration;
+      thread_args[i].maxResiduum = &maxResiduum;
+      thread_args[i].continue_iteration = &continue_iteration;
+      
+      /* alle Threads führen thread_calculate mit &thread_args durch(Argumente für die Funktion) */
+      pthread_create(&threads[i], NULL, thread_calculate, &thread_args[i]);
+    }
+    
+    /* alle Threads joinen */
+    for (int i = 0; i < num_threads; i++) {
+      pthread_join(threads[i], NULL);
+    }
+    
+    /* Speicher und Barrier freigeben */
+    pthread_barrier_destroy(&barrier);
+    free(threads);
+    free(thread_args);
+    
+    results->m = m2;
+  }
 }
 
 /* ************************************************************************ */
